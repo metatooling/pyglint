afb2fc021ec21e7c70c5923580b0e2c1139e614f
metatooling <metatooling@cordaz.com>
2020-01-17 09:34:48 +0000

    Fix bumpversion

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ project = "pyglint"
 year = "2020"
 author = "metatooling"
 copyright = "{0}, {1}".format(year, author)
-version = release = "0.1.0"
+version = release = "0.1.1"
 
 pygments_style = "trac"
 templates_path = ["."]
